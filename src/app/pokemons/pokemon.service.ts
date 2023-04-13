0986288239174a789e5a41216cbb3dd568241a24
Blackjuker <ngabgnaarnaud2@gmail.com>
2023-04-13 12:25:55 +0200

    Observable starting 2

--- src/app/pokemons/pokemon.service.ts
+++ src/app/pokemons/pokemon.service.ts
@@ -18,8 +18,29 @@ export class PokemonService {
     //     return POKEMONS;
     // }
 
+  private log(log:string) {
+    console.log(log);
+  }
+
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      console.log(`${operation} failed: ${error.message}`)
+
+      return of(result as T);
+    }
+  }
+
+
+// Le pipe async est un pipe capable de consommer des Observables ( ou Promise) en appelant
+// implicitement la méthode subscribe (ou then) afin de "binder" les valeurs contenus dans 
+//l'observable (ou la promise)
     getPokemons():Observable<Pokemon[]> {
-      return this.http.get<Pokemon[]>(this.pokemonsUrl)
+      return this.http.get<Pokemon[]>(this.pokemonsUrl).pipe(
+      tap(_ =>this.log(`fetched pokemons`)),
+      catchError(this.handleError(`getPokemons`,[]))
+      );
     }
 
     // getPokemons():Observable<any> {
@@ -27,14 +48,22 @@ export class PokemonService {
     // }
     
     //Retourne le pokémon avec l'identifiant passé en paramètre
-    getPokemon(id:number){
-        let pokemons = POKEMONS
-        for(let i=0;i<pokemons.length;i++){
-            if(pokemons[i].id == id){
-              return pokemons[i]
-            }
-          }
-          return false;
+    // getPokemon(id:number){
+    //     let pokemons = POKEMONS
+    //     for(let i=0;i<pokemons.length;i++){
+    //         if(pokemons[i].id == id){
+    //           return pokemons[i]
+    //         }
+    //       }
+    //       return false;
+    // }
+
+    getPokemon(id:number):Observable<Pokemon> {
+      const url = `${this.pokemonsUrl}/${id}`;
+      return this.http.get<Pokemon>(url).pipe(
+      tap(_ =>this.log(`fetched pokemon ${id}`)),
+      catchError(this.handleError<Pokemon>(`getPokemon id=${id}`))
+      );
     }
 
     //Delete Pokemon by id  
